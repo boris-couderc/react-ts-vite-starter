47653e47ff3059f2d4092b5bd7f13ab9c72dde9d
boris-couderc <couderc.boris@gmail.com>
2023-07-24 01:22:20 +0200

    add postcss and basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import '~/styles/index.pcss'
 
 import { IconReact, IconAccount } from './icons/Icon'
+
+import viteLogo from '/vite.svg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,18 +15,19 @@ function App() {
         <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
           <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
       </div>
       <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='home'>
+        <div className='home_card'>
+          <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat.
+          </p>
+        </div>
+        <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
       </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
 
       <IconReact color='primary' />
       <IconAccount color='secondary' />
